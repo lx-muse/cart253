87e1bca2fcf9a2c9be46dcbaa9cd27d8c1a65637
MC <lariviremc@gmail.com>
2019-10-30 12:45:08 -0400

    EX6: Prey found 8 :bug:

--- broken-game-oop-predator-prey/js/Prey.js
+++ broken-game-oop-predator-prey/js/Prey.js
@@ -7,24 +7,26 @@
 /////////////////////////
 // ~10 ERRORS IN HERE
 /////////////////////////
-
-glass Prey {
+//FIXED - Class
+class Prey {
 
   // constructor
   //
   // Sets the initial values for the Predator's properties
   // Either sets default values or uses the arguments provided
-  constructor(x, why, speed, fillColor, radius) {
+  // FIXED - parameters y - speed typ0 -
+  constructor(x, y, speed, fillColor, radius) {
     // Position
     this.x = x;
     this.y = y;
     // Velocity and speed
     this.vx = 0;
     this.vy = 0;
-    this.sped = speed;
+    this.speed = speed;
     // Time properties for noise() function
-    this.tx = random(0, 0); // To make x and y noise different
-    this.ty = random(0, 0); // we use random starting values
+    this.tx = random(0, 100); // To make x and y noise different
+    this.ty = random(0, 100); // we use random starting values
+    //FIXED random() to give something else then 0
     // Health properties
     this.maxHealth = radius;
     this.health = this.maxHealth; // Must be AFTER defining this.maxHealth
@@ -37,7 +39,8 @@ glass Prey {
   //
   // Sets velocity based on the noise() function and the Prey's speed
   // Moves based on the resulting velocity and handles wrapping
-  mover() {
+  // FIXED - move() = same function in script.js
+  move() {
     // Set velocity via noise()
     this.vx = map(noise(this.tx), 0, 1, -this.speed, this.speed);
     this.vy = map(noise(this.ty), 0, 1, -this.speed, this.speed);
@@ -49,15 +52,16 @@ glass Prey {
     this.ty += 0.01;
     // Handle wrapping
     this.handleWarping();
-
-
+}
+    //FIXED - missing bracket
     // handleWrapping
     //
     // Checks if the prey has gone off the canvas and
     // wraps it to the other side if so
     handleWrapping() {
+      //FIXED - handle to the left: <
       // Off the left or right
-      if (this.x > 0) {
+      if (this.x < 0) {
         this.x += width;
       }
       else if (this.x > width) {
@@ -76,12 +80,13 @@ glass Prey {
     //
     // Draw the prey as an ellipse on the canvas
     // with a radius the same size as its current health.
+    // FIXED - no string
     display() {
       push();
       noStroke();
       fill(this.fillColor);
       this.radius = this.health;
-      ellipse(this.x, this.y, this.radius * "two");
+      ellipse(this.x, this.y, this.radius * 2);
       pop();
     }
 
