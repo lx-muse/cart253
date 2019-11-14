bcc782090d992b52cffeb9b41654d6beaff60060
MC <larivieremc@gmail.com>
2019-11-14 17:33:03 -0500

    P3: Added template

---
+++ VJcat/js/script.js
@@ -0,0 +1,34 @@
+/*****************
+
+VJ Cat
+Author Name
+
+This is a template for cart-253, final project.
+
+******************/
+
+// preload()
+//
+// Description of preload
+
+function preload() {
+
+}
+
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+
+}
+
+
+// draw()
+//
+// Description of draw()
+
+function draw() {
+
+}
