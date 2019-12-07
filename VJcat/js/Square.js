5a859d6eb27c0c87d2690c14fd63a841f19c691d
MC <larivieremc@gmail.com>
2019-12-06 19:39:14 -0500

    P3: commenting

--- VJcat/js/Square.js
+++ VJcat/js/Square.js
@@ -2,22 +2,22 @@ class Square extends Shape {
   constructor(x,y,size) {
     super(x,y,size);
   }
-
+  // Display a square a big as keyboar keys (for feedback)
   display() {
     push();
     rectMode(CENTER);
-    fill('rgba(204,204,255, 0.25)');
+    fill('rgba(204,204,255, 0.15)');
     noStroke();
     rect(this.x,this.y,this.size,this.size);
     pop();
   }
 
-  hide() {
-    push();
-    rectMode(CENTER);
-    fill('rgba(204,204,255, 0)');
-    noStroke();
-    rect(this.x,this.y,this.size,this.size);
-    pop();
-  }
+  // hide() {
+  //   push();
+  //   rectMode(CENTER);
+  //   fill('rgba(204,204,255, 0)');
+  //   noStroke();
+  //   rect(this.x,this.y,this.size,this.size);
+  //   pop();
+  // }
 }
